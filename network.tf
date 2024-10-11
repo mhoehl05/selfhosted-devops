232dc675d20550411cdcf0ed31454d5be8e8bdc7
Höhl <MHOEHL@controlware.de>
2024-10-11 10:15:08 +0200

    added service endpoint for storage to agent subnet

--- network.tf
+++ network.tf
@@ -17,4 +17,5 @@ resource "azurerm_subnet" "agents" {
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.0.64/27"]
+  service_endpoints    = ["Microsoft.Storage"]
 }
