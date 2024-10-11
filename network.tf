b2cfcd0673d7c149b5bb7003d729072c58577b1d
Höhl <MHOEHL@controlware.de>
2024-10-11 09:42:38 +0200

    fixed subnet ranges

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "main" {
   name                = "vnet-devops-demo-weu"
-  address_space       = ["10.0.0.0/26"]
+  address_space       = ["10.0.0.0/23"]
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 }
@@ -9,12 +9,12 @@ resource "azurerm_subnet" "bastion" {
   name                 = "AzureBastionSubnet"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/26"]
+  address_prefixes     = ["10.0.0.0/26"]
 }
 
 resource "azurerm_subnet" "agents" {
   name                 = "DevopsAgentSubnet"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.128/27"]
+  address_prefixes     = ["10.0.0.64/27"]
 }
