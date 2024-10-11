779d41dea29ad0c78744c7915f8a051e90677fdb
Höhl <MHOEHL@controlware.de>
2024-10-11 11:11:06 +0200

    added vmss to network

---
+++ agents.tf
@@ -0,0 +1,39 @@
+resource "azurerm_linux_virtual_machine_scale_set" "agents" {
+  name                = "vmss-devopsagents-demo-weu"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  sku                 = "Standard_B2s"
+  instances           = 2
+  admin_username      = "adm_ubuntu"
+  overprovision       = false
+  upgrade_mode        = "Manual"
+  single_placement_group = false
+
+  admin_ssh_key {
+    username   = "adm_ubuntu"
+    public_key = file("${path.module}/ssh_keys/id_rsa.pub")
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "StandardSSD_LRS"
+    caching              = "ReadWrite"
+  }
+
+  network_interface {
+    name    = "nic-devopsagents-demo-weu"
+    primary = true
+
+    ip_configuration {
+      name      = "internal"
+      primary   = true
+      subnet_id = azurerm_subnet.agents.id
+    }
+  }
+}
