23f183762a6dcb6b629cb5f23d22cae8db2ed86e
Höhl <MHOEHL@controlware.de>
2024-10-16 09:40:00 +0200

    changed rg resource name and removed config for vmss

--- data.tf
+++ data.tf
@@ -1,8 +1,8 @@
-data "azurerm_resource_group" "main" {
+data "azurerm_resource_group" "devops_rg" {
   name = "rg-devops-demo-weu"
 }
 
 data "azurerm_storage_account" "state_deposit" {
   name                = "staccdefaultstatedeposit"
-  resource_group_name = data.azurerm_resource_group.main.name
+  resource_group_name = data.azurerm_resource_group.devops_rg.name
 }
