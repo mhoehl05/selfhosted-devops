48b45d67f82389ceabf1fcf5da88abfd1a783f78
Höhl <MHOEHL@controlware.de>
2024-10-16 09:41:52 +0200

    changed vnet resource name and fixed reference on rg

--- network.tf
+++ network.tf
@@ -1,22 +1,22 @@
-resource "azurerm_virtual_network" "main" {
+resource "azurerm_virtual_network" "devops_vnet" {
   name                = "vnet-devops-demo-weu"
   address_space       = ["10.1.0.0/23"]
-  location            = data.azurerm_resource_group.main.location
-  resource_group_name = data.azurerm_resource_group.main.name
+  location            = data.azurerm_resource_group.devops_rg.location
+  resource_group_name = data.azurerm_resource_group.devops_rg.name
 }
 
 resource "azurerm_subnet" "agents" {
   name                 = "DevopsAgentSubnet"
-  resource_group_name  = data.azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
+  resource_group_name  = data.azurerm_resource_group.devops_rg.name
+  virtual_network_name = azurerm_virtual_network.devops_vnet.name
   address_prefixes     = ["10.1.0.0/27"]
   service_endpoints    = ["Microsoft.Storage"]
 }
 
 resource "azurerm_subnet" "system" {
   name                 = "SystemSubnet"
-  resource_group_name  = data.azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
+  resource_group_name  = data.azurerm_resource_group.devops_rg.name
+  virtual_network_name = azurerm_virtual_network.devops_vnet.name
   address_prefixes     = ["10.1.0.32/27"]
 }
 
