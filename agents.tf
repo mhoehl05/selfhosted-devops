b870309abde0a408811aedad16fe449e6936839a
Höhl <MHOEHL@controlware.de>
2024-10-16 16:32:15 +0200

    changed to count for agents

--- agents.tf
+++ agents.tf
@@ -1,50 +1,27 @@
-locals {
-  agents = [
-    {
-      name   = "agent-1"
-      cpu    = "0.5"
-      memory = "1.5"
-    },
-    {
-      name   = "agent-2"
-      cpu    = "0.5"
-      memory = "1.5"
-    },
-    {
-      name   = "agent-3"
-      cpu    = "0.5"
-      memory = "1.5"
-    }
-  ]
-}
-
-
 resource "azurerm_container_group" "agents" {
-  name                = "continst-tfcagent-demo-weu"
+  count = 3
+
+  name                = "continst-tfcagent-demo-weu-${count.index + 1}"
   location            = data.azurerm_resource_group.devops_rg.location
   resource_group_name = data.azurerm_resource_group.devops_rg.name
   ip_address_type     = "Private"
   os_type             = "Linux"
   subnet_ids          = ["${azurerm_subnet.agents.id}"]
 
-  dynamic "container" {
-    for_each = local.agents
+  container {
+    name   = "tfcagent"
+    image  = "docker.io/hashicorp/tfc-agent:latest"
+    cpu    = container.value.cpu
+    memory = container.value.memory
 
-    content {
-      name   = "container-tfcagent-demo-weu-${index(local.agents, container.value) + 1}"
-      image  = "docker.io/hashicorp/tfc-agent:latest"
-      cpu    = container.value.cpu
-      memory = container.value.memory
+    secure_environment_variables = {
+      "TFC_AGENT_TOKEN" = "${var.TFC_AGENT_TOKEN}"
+      "TFC_AGENT_NAME"  = "${var.TFC_AGENT_NAME}-${count.index + 1}"
+    }
 
-      secure_environment_variables = {
-        "TFC_AGENT_TOKEN" = "${var.TFC_AGENT_TOKEN}"
-        "TFC_AGENT_NAME"  = "${var.TFC_AGENT_NAME}-${index(local.agents, container.value) + 1}"
-      }
-
-      ports {
-        port     = 22
-        protocol = "TCP"
-      }
+    ports {
+      port     = 22
+      protocol = "TCP"
     }
   }
 }
