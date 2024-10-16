0f57969c05eaa306780fd729c5dab2f9fcf30d13
Höhl <MHOEHL@controlware.de>
2024-10-16 12:53:40 +0200

    open ssh port for debugging

--- agents.tf
+++ agents.tf
@@ -2,7 +2,7 @@ resource "azurerm_container_group" "agents" {
   name                = "continst-tfcagent-demo-weu"
   location            = data.azurerm_resource_group.devops_rg.location
   resource_group_name = data.azurerm_resource_group.devops_rg.name
-  ip_address_type     = "private"
+  ip_address_type     = "Private"
   os_type             = "Linux"
   subnet_ids          = ["${azurerm_subnet.agents.id}"]
 
@@ -15,6 +15,11 @@ resource "azurerm_container_group" "agents" {
     secure_environment_variables = {
       "TFC_AGENT_TOKEN" = "${var.TFC_AGENT_TOKEN}"
       "TFC_AGENT_NAME"  = "${var.TFC_AGENT_NAME}"
+    }
+
+    ports {
+      port     = 22
+      protocol = "TCP"
     }
   }
 }
