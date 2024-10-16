38f9a118b90cd0c4cf043e718be91dd2967a5433
Höhl <MHOEHL@controlware.de>
2024-10-16 09:37:33 +0200

    created acr and changed network space

--- network.tf
+++ network.tf
@@ -1,23 +1,23 @@
 resource "azurerm_virtual_network" "main" {
   name                = "vnet-devops-demo-weu"
-  address_space       = ["10.0.0.0/23"]
+  address_space       = ["10.1.0.0/23"]
   location            = data.azurerm_resource_group.main.location
   resource_group_name = data.azurerm_resource_group.main.name
-}
-
-resource "azurerm_subnet" "bastion" {
-  name                 = "AzureBastionSubnet"
-  resource_group_name  = data.azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.0.0/26"]
 }
 
 resource "azurerm_subnet" "agents" {
   name                 = "DevopsAgentSubnet"
   resource_group_name  = data.azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.0.64/27"]
+  address_prefixes     = ["10.1.0.0/27"]
   service_endpoints    = ["Microsoft.Storage"]
+}
+
+resource "azurerm_subnet" "system" {
+  name                 = "SystemSubnet"
+  resource_group_name  = data.azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.1.0.32/27"]
 }
 
 resource "azurerm_storage_account_network_rules" "default" {
---
+++ registry.tf
@@ -0,0 +1,61 @@
+resource "azurerm_container_registry" "agent_acr" {
+  name                = "acr-tfcagents-demo-weu"
+  resource_group_name = data.azurerm_resource_group.devops_rg.name
+  location            = data.azurerm_resource_group.devops_rg.location
+  sku                 = "Premium"
+  admin_enabled       = true
+
+  network_rule_set {
+    default_action = "Deny"
+  }
+}
+
+resource "azurerm_private_endpoint" "acr_pep" {
+  name                = "pep-acr-demo-weu"
+  resource_group_name = data.azurerm_resource_group.devops_rg.name
+  location            = data.azurerm_resource_group.devops_rg.location
+  subnet_id           = azurerm_subnet.system.id
+
+  private_service_connection {
+    name                           = "pepconn-acr-demo-weu"
+    private_connection_resource_id = azurerm_container_registry.agent_acr.id
+    is_manual_connection           = false
+  }
+}
+
+data "azurerm_network_interface" "acr_nic" {
+  name                = azurerm_private_endpoint.acr_pep.network_interface[0].name
+  resource_group_name = data.azurerm_resource_group.devops_rg.name
+
+  depends_on = [
+    azurerm_private_endpoint.acr_pep
+  ]
+}
+
+resource "azurerm_private_dns_zone" "acr_dns" {
+  name                = "privatelink.azurecr.io"
+  resource_group_name = data.azurerm_resource_group.devops_rg.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "dns_vnet_link" {
+  name                  = "dnslink-acr-demo-weu"
+  resource_group_name   = data.azurerm_resource_group.devops_rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.acr_dns.name
+  virtual_network_id    = var.vnet_id
+}
+
+resource "azurerm_private_dns_a_record" "pep_dns_record_data" {
+  name                = lower(format("%s.%s.data", azurerm_container_registry.agent_acr.name, data.azurerm_resource_group.devops_rg.location))
+  zone_name           = azurerm_private_dns_zone.acr_dns.name
+  resource_group_name = var.resource_group_name
+  ttl                 = 3600
+  records             = [data.azurerm_network_interface.acr_nic.private_ip_addresses[0]]
+}
+
+resource "azurerm_private_dns_a_record" "pep_dns_record" {
+  name                = lower(azurerm_container_registry.agent_acr.name)
+  zone_name           = azurerm_private_dns_zone.acr_dns.name
+  resource_group_name = data.azurerm_resource_group.devops_rg.name
+  ttl                 = 3600
+  records             = [data.azurerm_network_interface.acr_nic.private_ip_addresses[1]]
+}
