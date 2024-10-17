dc15665b78d2753503ee2ddaf5b67f22b8744a1d
Höhl <MHOEHL@controlware.de>
2024-10-17 10:45:17 +0200

    removed unused task folder and add jumphost for debugging

---
+++ jumphost.tf
@@ -0,0 +1,7 @@
+module "jumphost" {
+  source = "git::https://github.com/mhoehl05/basic-jumphost.git?ref=main"
+
+  rg_name         = data.azurerm_resource_group.hub_rg.name
+  snet_id         = azurerm_subnet.system.id
+  public_key_path = "${path.module}/ssh_keys/id_rsa_mark.pub"
+}
