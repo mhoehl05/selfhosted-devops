795df210563dcd87297b9456815b58a8d396d1fb
Höhl <MHOEHL@controlware.de>
2024-10-17 10:46:25 +0200

    move jumphost to tfcagent subnet

--- jumphost.tf
+++ jumphost.tf
@@ -2,6 +2,6 @@ module "jumphost" {
   source = "git::https://github.com/mhoehl05/basic-jumphost.git?ref=main"
 
   rg_name         = data.azurerm_resource_group.hub_rg.name
-  snet_id         = azurerm_subnet.system.id
+  snet_id         = azurerm_subnet.tfc_agents.id
   public_key_path = "${path.module}/ssh_keys/id_rsa_mark.pub"
 }
