4e4d4c6d07508b560b819278657c761ad9a4bbb9
Höhl <MHOEHL@controlware.de>
2024-10-17 08:55:58 +0200

    added task to pull tfc agent image

--- lifecycle.tf
+++ lifecycle.tf
@@ -7,3 +7,24 @@ resource "azurerm_container_registry_agent_pool" "acr_agents" {
   tier                      = "S1"
   virtual_network_subnet_id = azurerm_subnet.acr_agents.id
 }
+
+resource "azurerm_container_registry_task" "pull_tfcagent" {
+  name                  = "pull-tfcagent-image"
+  container_registry_id = azurerm_container_registry.base_acr.id
+  agent_pool_name       = azurerm_container_registry_agent_pool.acr_agents.name
+  
+  platform {
+    os = "Linux"
+  }
+
+  file_step {
+    task_file_path = "${path.module}/tasks/acragents.yaml"
+    values = {
+      "REGISTRY_FROM_URL" = "docker.io"
+    }
+  }
+}
+
+resource "azurerm_container_registry_task_schedule_run_now" "pull_tfcagent" {
+  container_registry_task_id = azurerm_container_registry_task.pull_tfcagent.id
+}
