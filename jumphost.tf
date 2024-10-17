6069f94652d17abeeb24fbcc4092d9f819c3ae24
Höhl <MHOEHL@controlware.de>
2024-10-17 10:47:09 +0200

    fixed rg reference

--- jumphost.tf
+++ jumphost.tf
@@ -1,7 +1,7 @@
 module "jumphost" {
   source = "git::https://github.com/mhoehl05/basic-jumphost.git?ref=main"
 
-  rg_name         = data.azurerm_resource_group.hub_rg.name
+  rg_name         = data.azurerm_resource_group.devops_rg.name
   snet_id         = azurerm_subnet.tfc_agents.id
   public_key_path = "${path.module}/ssh_keys/id_rsa_mark.pub"
 }
