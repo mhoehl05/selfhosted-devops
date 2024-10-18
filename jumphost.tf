c0592a3702fc006020adabd1f742406b70c410f9
Höhl <MHOEHL@controlware.de>
2024-10-18 10:17:46 +0200

    remove jumphost

--- jumphost.tf
+++
@@ -1,7 +0,0 @@
-module "jumphost" {
-  source = "git::https://github.com/mhoehl05/basic-jumphost.git?ref=main"
-
-  rg_name         = data.azurerm_resource_group.devops_rg.name
-  snet_id         = azurerm_subnet.system.id
-  public_key_path = "${path.module}/ssh_keys/id_rsa_mark.pub"
-}
