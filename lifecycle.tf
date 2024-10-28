770d9db1f1a699d18466583f303444aea73d5f4e
Höhl <MHOEHL@controlware.de>
2024-10-28 11:19:25 +0100

    use dedicated hosts for runs

--- lifecycle.tf
+++ lifecycle.tf
@@ -1,4 +1,4 @@
-/* resource "azurerm_container_registry_agent_pool" "acr_agents" {
+resource "azurerm_container_registry_agent_pool" "acr_agents" {
   name                      = "base-acr-agentpool"
   resource_group_name       = data.azurerm_resource_group.devops_rg.name
   location                  = data.azurerm_resource_group.devops_rg.location
@@ -6,12 +6,12 @@
   instance_count            = 1
   tier                      = "S1"
   virtual_network_subnet_id = azurerm_subnet.acr_agents.id
-} */
+}
 
 resource "azurerm_container_registry_task" "pull_tfcagent" {
   name                  = "pull-tfcagent-image"
   container_registry_id = azurerm_container_registry.base_acr.id
-  #agent_pool_name       = azurerm_container_registry_agent_pool.acr_agents.name
+  agent_pool_name       = azurerm_container_registry_agent_pool.acr_agents.name
 
   platform {
     os = "Linux"
